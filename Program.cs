14f2080d0fdd7c1239aef4ca688fc6f6c7f36685
Vallem Balu <vbnreddy5@gmail.com>
2021-07-05 15:35:36 +0530

    Player check option

--- Program.cs
+++ Program.cs
@@ -4,18 +4,37 @@ namespace Snake
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("***Welcome To Snake and Ladder Gmae***");
-            int playerPosition = 0;
-            int playerDice = rollDice();
-            Console.WriteLine("Roll Dice: "  + playerDice);
-        }
         public static int rollDice()
         {
             Random random = new Random();
             int dice = random.Next(1, 7);
             return dice;
+        }
+        public static int checkOption()
+        {
+            Random random = new Random();
+            int option = random.Next(1, 4);
+            return option;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Welcome To Snake and Ladder Gmae***");
+            int playerPosition = 0;
+
+            int playerDice = rollDice();
+            int option = checkOption();
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Player Got Ladder.");
+                    break;
+                case 2:
+                    Console.WriteLine("Player got Snake.");
+                    break;
+                default:
+                    Console.WriteLine("Player not playing.");
+                    break;
+            }
         }
     }
 }
